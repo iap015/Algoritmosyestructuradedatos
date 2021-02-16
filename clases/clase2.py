b051df7a8ddea86792999aed2cf2ac091fac07d4
iap015 <isarangoparra@gmail.com>
2021-02-15 22:49:45 -0500

    Actualizando

---
+++ clases/clase2.py
@@ -0,0 +1,24 @@
+pruebaT = True
+pruebaF = False 
+print (pruebaT) 
+print (pruebaF)   
+edad = 18
+estatura = 1.66
+peso = 50
+nombre = "Isabella Parra"
+# vamos a ver si la edad es mayor a 15
+print ("#"*15, "mayor edad", "#"*15)
+isMayorEdad = edad >= 18
+print (isMayorEdad)
+# calculando la estatura promedio
+print ("#"*15, "bajo la estatura promedio", "#"*15)
+isMayorEstatura = estatura < 1.70
+print (isMayorEstatura)
+# calculando si el peso es diferente a 50 
+print ("#"*15, "peso diferente 50", "#"*15)
+isPesoDiferente = peso != 50
+print (isPesoDiferente)
+# vamos a ver si un apellido esta en el nombre completo 
+apellido = "Parra"
+isApellido = apellido in nombre
+print (isApellido)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
