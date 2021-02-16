65f8408195f73f4cd59daae286b8244a8c002829
iap015 <isarangoparra@gmail.com>
2021-02-16 00:04:29 -0500

    taller

---
+++ talleres/taller#1.py
@@ -0,0 +1,31 @@
+edadIsabella = 18
+edadMariana = 20
+# estamos sumando las edad de isabella y mariana
+sumar = edadIsabella + edadMariana
+print (f"la suma dio {sumar} correctamente")
+# estamos restando las edades de isabella y mariana
+restar = edadIsabella - edadMariana
+print (f"la resta dio {restar} correctamente")
+# estamos multiplicando las edades de isabella y mariana
+multiplicar = edadIsabella * edadMariana
+print (f"la multiplicacion dio {multiplicar} correctamente")
+# estamos diviendo las edades de isabella y mariana
+dividir = edadIsabella / edadMariana
+print (f"la division dio {dividir} correctamente")
+estaturaIsabella = 1.64
+estaturaJhonatan = 1.74
+estaturaMariana = 1.65
+# vamos ver si isabella es mas alta que jhonatan
+print ("#"*10, "es isabella mas alta que jhonatan", "#"*10)
+isMayorEstatura = estaturaIsabella > estaturaJhonatan
+print (isMayorEstatura)
+# vamos a ver el promedio de las estaturas 
+print ("#"*10, "promedio de estaturas", "#"*10)
+isEstaturaPromedio = P = (estaturaIsabella + estaturaJhonatan + estaturaMariana) / 3
+print (isEstaturaPromedio)
+
+
+
+
+
+
