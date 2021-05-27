6f98da75d791db7080d6164387cb6d0ced5e3bf4
iap015 <isarangoparra@gmail.com>
2021-05-27 08:19:48 -0500

    mensaje de despedida

---
+++ mensajededespedida.py
@@ -0,0 +1,6 @@
+#MENSAJE
+
+#Profe muchas gracias por todo, por tantos aprendizajes durante todo el semestre y sobre todo por tu paciencia tan grande
+#espero que nos veamos de nuevo porque eres un gran profesor y una gran persona.
+#Feliz dia profe y que tengas un excelente resto de año.
+#*,*
